c131617b4f33cf568edb27a61b68e2e037d3cc4f
MarcoAntonioMontez <marco.montez.lis@gmail.com>
2020-04-15 16:55:47 +0100

    tweak

--- flask-frontend-ml/webapp/nametranslator.py
+++ flask-frontend-ml/webapp/nametranslator.py
@@ -12,5 +12,11 @@ def translator(name):
 		return 'Joao\'s are usually dog lovers and very conscious about going to sleep early'
 	elif _name == 'jose':
 		return 'These younger brothers have still much to learn from their older senseis'
+	elif _name == 'manuel' or _name == 'manel':
+		return 'Alegedly a monk in a past life, today he occupies itself with more earthly occupations, ranging from house investing to fungus plantations'
+	elif _name == 'filipe':
+		return 'An enigmatic character indeed, some call him Captain. And legend says he is rebooting'
+	elif _name == 'duarte':
+		return 'Apparently a very innocent chap but a "killer" on the back seat of a car'
 	else:
-		return 'No translation available'
+		return 'No translation available, try another name!' 
